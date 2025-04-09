6bf650b4da863a43966262b7f1272795acf60369
natalia <natalia.maslij1@gmail.com>
2025-04-09 14:26:25 +0200

    listening to firebase auth state changes with authStateProvider, basic screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,31 @@
+import 'package:auth_firebase/providers/auth_state_provider.dart';
+import 'package:auth_firebase/screens/auth_screen.dart';
+import 'package:auth_firebase/screens/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MaterialApp(home: MainApp())));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authState = ref.watch(authStateProvider);
+
+    return authState.when(
+        data: (user) {
+          if (user == null) return const AuthScreen();
+          return const HomeScreen();
+        },
+        error: (e, s) => const Placeholder(),
+        loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ));
   }
 }
