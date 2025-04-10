fdef4cfb1f863b7ded4e629c927cc8ef4d7cd4f8
natalia <natalia.maslij1@gmail.com>
2025-04-10 03:29:05 +0200

    haveAccountProvidr, switch for auth screens

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,12 +1,26 @@
+import 'package:auth_firebase/providers/have_account_provider.dart';
+import 'package:auth_firebase/screens/login_screen.dart';
+import 'package:auth_firebase/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends ConsumerWidget {
   const AuthScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('auth')),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bool haveAccount = ref.watch(haveAccountProvider);
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Switch(
+            value: haveAccount,
+            onChanged: (value) =>
+                ref.read(haveAccountProvider.notifier).state = value,
+          )
+        ],
+      ),
+      body: haveAccount ? const LoginScreen() : const RegistrationScreen(),
     );
   }
 }
